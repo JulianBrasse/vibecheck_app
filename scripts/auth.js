2366301b5b1557a039495bda7e179de472c3c9ce
Julian Brasse <julian.brasse@gmail.com>
2025-12-11 12:19:50 +0100

    Simplify supabase initialization

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,7 +1,8 @@
-// 1. Initialize Supabase Client
-const supabaseUrl = "https://xnfmgcnmdscyyaytacgp.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZm1nY25tZHNjeXlheXRhY2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODkyMTIsImV4cCI6MjA2ODg2NTIxMn0.pY02zXDHqGpauVf1iTaiRsOl1i-K6upjWc9NMQmdSjM";
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+import { createClient } from "@supabase/supabase-js";
+
+const supabase_url = "https://xnfmgcnmdscyyaytacgp.supabase.co";
+const supabase_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZm1nY25tZHNjeXlheXRhY2dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyODkyMTIsImV4cCI6MjA2ODg2NTIxMn0.pY02zXDHqGpauVf1iTaiRsOl1i-K6upjWc9NMQmdSjM";
+const supabase = createClient(supabase_url, supabase_key)
 
 // 2. Select Elements
 const emailInput = document.getElementById("email");
