8aab887c30c503a3486de1f5d8bb061ef4e62bba
Julian Brasse <julian.brasse@gmail.com>
2025-12-13 01:09:41 +0100

    Small syntax fix.

--- scripts/vibecheck.js
+++ scripts/vibecheck.js
@@ -2,15 +2,16 @@ import { supabase } from "/scripts/supabase.js";
 
 const happiness_scale = document.getElementById("happiness_scale");
 const submit_vibe_button = document.getElementById("submit_vibe_button");
-const happiness_scale_number = document.getElementById("happiness_scale_number);"
+const happiness_scale_number = document.getElementById("happiness_scale_number)";
+
 async function submitVibe() {
-    const happiness_level = (happiness_scale.valueAsNumber / 4);
-    alert(happiness_level);
+	const happiness_level = (happiness_scale.valueAsNumber / 4);
+	alert(happiness_level);
 }
 
 async function showVibe() {
-    const happiness_level = (happiness_scale.valueAsNumber / 4);
-    happiness_scale_number.innerText = happiness_level + " / 10";
+	const happiness_level = (happiness_scale.valueAsNumber / 4);
+	happiness_scale_number.innerText = happiness_level + " / 10";
 }
 
 if (submit_vibe_button) {
