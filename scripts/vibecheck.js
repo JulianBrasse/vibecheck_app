3c9bf370c7fa66db792d8331492686b043e96e30
Julian Brasse <julian.brasse@gmail.com>
2025-12-13 01:07:03 +0100

    VibeCheck updates.

--- scripts/vibecheck.js
+++ scripts/vibecheck.js
@@ -2,12 +2,21 @@ import { supabase } from "/scripts/supabase.js";
 
 const happiness_scale = document.getElementById("happiness_scale");
 const submit_vibe_button = document.getElementById("submit_vibe_button");
-
+const happiness_scale_number = document.getElementById("happiness_scale_number);"
 async function submitVibe() {
-    const happiness_level = (happiness_scale.valueAsNumber / 40);
+    const happiness_level = (happiness_scale.valueAsNumber / 4);
     alert(happiness_level);
+}
+
+async function showVibe() {
+    const happiness_level = (happiness_scale.valueAsNumber / 4);
+    happiness_scale_number.innerText = happiness_level + " / 10";
 }
 
 if (submit_vibe_button) {
 	submit_vibe_button.addEventListener("click", submitVibe);
+}
+
+if (happiness_scale) {
+	happiness_scale.addEventListener("input", showVibe);
 }
